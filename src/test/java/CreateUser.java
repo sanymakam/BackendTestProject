55ea2d6d6d54c077f895e53a28be8fef044e862c
Santhosh Makam <santhoshmakam@Santhosh-Makam.local>
2024-03-26 22:38:02 +0530

    Working code as testng and gradle

--- src/test/java/CreateUser.java
+++ src/test/java/CreateUser.java
@@ -1,10 +1,12 @@
 import controllers.UserControllers;
 import io.restassured.response.Response;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class CreateUser {
-    public static void main(String[] args) {
+    @Test(groups = "users")
+    public void createDummyUser() {
         UserControllers userControllers = new UserControllers();
         Response response = userControllers.createUser();
         response.body().prettyPrint();
