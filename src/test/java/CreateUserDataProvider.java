c71b20b4781058c7232a460f6cd882951afb78c2
Santhosh Makam <santhoshmakam@Santhosh-Makam.local>
2024-03-27 07:32:17 +0530

    Running tests in parallel

--- src/test/java/CreateUserDataProvider.java
+++ src/test/java/CreateUserDataProvider.java
@@ -2,8 +2,12 @@ import org.testng.annotations.DataProvider;
 
 public class CreateUserDataProvider {
 
-    @DataProvider(name = "createUserData")
+    @DataProvider(name = "createUserData",parallel = true)
     public Object[][] createUserData() {
-        return new Object[][] {{"Morpheus","Lead"},{"James","Manager"}};
+        return new Object[][] {{"Morpheus","Lead"},
+                {"James","Manager"},
+                {"Mike","Employee"},
+                {"Bob","Employee"},
+                {"Justin","CEO"}};
     }
 }
