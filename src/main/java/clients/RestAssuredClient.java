96943ad887affa515850ff5fd59b0ff39a67501d
Santhosh Makam <santhoshmakam@Santhosh-Makam.local>
2024-03-28 07:35:28 +0530

    proper segregation to client

--- src/main/java/clients/RestAssuredClient.java
+++ src/main/java/clients/RestAssuredClient.java
@@ -1,22 +1,22 @@
-package base;
+package clients;
 
-import clients.RestAssuredClient;
 import io.restassured.response.Response;
 
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
-public class BaseRequests {
+public class RestAssuredClient {
+
     public static Response post(String url, Map<String, String> headers, Object payLoad) {
-        return RestAssuredClient.post(url,headers,payLoad);
+        return given().headers(headers).body(payLoad).when().post(url).then().extract().response();
     }
 
     public static Response get(String url) {
-        return RestAssuredClient.get(url);
+        return given().urlEncodingEnabled(false).when().get(url).then().extract().response();
     }
 
     public static Response patch(String url, Map<String, String> headers) {
-        return RestAssuredClient.patch(url,headers);
+        return given().headers(headers).when().patch(url).then().extract().response();
     }
 }
